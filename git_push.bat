90b15077a18828a2ff1e05ba0352089a612ea7ae
arun-r-007 <arunakhil9715@gmail.com>
2025-09-03 08:01:30 +0530

    LeetCode

---
+++ git_push.bat
@@ -0,0 +1,13 @@
+@echo off
+git add .
+git commit -m "LeetCode"
+git push origin main
+pause
+
+
+@REM @echo off
+@REM set /p msg=Commit message: 
+@REM git add .
+@REM git commit -m "%msg%"
+@REM git push origin main
+@REM pause
