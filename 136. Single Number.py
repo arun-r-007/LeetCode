4e720e19747fdf30b91c30e4283ba2f8618b70b7
arun-r-007 <arunakhil9715@gmail.com>
2025-09-05 22:09:37 +0530

    LeetCode

---
+++ 136. Single Number.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
+
+
+        # if nums == []:
+        #     return NULL
+        # else :
+        #     n = len(nums)
+        #     ls = []
+        #     for i in range(n):
+        #         if nums[i] in ls:
+        #             ls.remove(nums[i])
+
+        #         else :
+        #             ls.append(nums[i])
+
+        #     return ls[0]
