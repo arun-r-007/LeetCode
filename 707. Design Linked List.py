2a42ca83b8c4cb4efe54af2148695b891f6f3b22
arun-r-007 <arunakhil9715@gmail.com>
2025-09-09 15:38:19 +0530

    LeetCode

--- 707. Design Linked List.py
+++ 707. Design Linked List.py
@@ -1,56 +1,72 @@
-class MyLinkedList(object):
-
-    def __init__(self,val = 0,next = None):
+class Node:
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
 
     def get(self, index):
-        """
-        :type index: int
-        :rtype: int
-        """
-        current = self
-        for i in range(index):
+        if index < 0 or index >= self.size:
+            return -1
+        current = self.head
+        for _ in range(index):
             current = current.next
         return current.val
-        
 
     def addAtHead(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        current = self
-        new_node = MyLinkedList(val)
-        new_node.next = current
-        self = new_node
-        
+        new_node = Node(val, self.head)
+        self.head = new_node
+        if self.size == 0:
+            self.tail = new_node
+        self.size += 1
 
     def addAtTail(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        while self.next:
-            self = self.next
-        self.next = MyLinkedList(val)
-        
+        new_node = Node(val)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
 
     def addAtIndex(self, index, val):
-        """
-        :type index: int
-        :type val: int
-        :rtype: None
-        """
-        
-        
+        if index < 0 or index > self.size:
+            return
+        if index == 0:
+            self.addAtHead(val)
+            return
+        if index == self.size:
+            self.addAtTail(val)
+            return
+        new_node = Node(val)
+        current = self.head
+        for _ in range(index - 1):
+            current = current.next
+        new_node.next = current.next
+        current.next = new_node
+        self.size += 1
 
     def deleteAtIndex(self, index):
-        """
-        :type index: int
-        :rtype: None
-        """
+        if index < 0 or index >= self.size:
+            return
+        if index == 0:
+            self.head = self.head.next
+            if self.size == 1:
+                self.tail = None
+        else:
+            current = self.head
+            for _ in range(index - 1):
+                current = current.next
+            current.next = current.next.next
+            if index == self.size - 1:
+                self.tail = current
+        self.size -= 1
+
         
 
 
