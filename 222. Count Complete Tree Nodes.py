728f2b2c20917cb90c0d657e9aaa1f269b9a903f
arun-r-007 <arunakhil9715@gmail.com>
2025-09-09 07:51:09 +0530

    LeetCode

--- 222. Count Complete Tree Nodes.py
+++ 222. Count Complete Tree Nodes.py
@@ -25,4 +25,39 @@ class Solution(object):
             return 2 ** left_depth - 1
         else:
             return 1 + self.countNodes(root.left) + self.countNodes(root.right)
-            
+        
+
+
+
+
+
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution(object):
+#     def countNodes(self, root):
+#         """
+#         :type root: Optional[TreeNode]
+#         :rtype: int
+#         """
+#         if root is None:
+#             return 0
+#         count = [0]
+
+#         def countN(node):
+#             if node is not None:
+#                 count[0] += 1
+#                 countN(node.left)
+#                 countN(node.right)
+        
+#         countN(root)
+#         return count[0]
