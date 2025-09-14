cd4e87c6cf610b915af6d48a77a49e59b11009f0
arun-r-007 <arunakhil9715@gmail.com>
2025-09-14 08:19:35 +0530

    LeetCode

---
+++ 483. Smallest Good Base.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def smallestGoodBase(self, n):
+        """
+        :type n: str
+        :rtype: str
+        """
+        n = int(n)
+
+        for m in range(int(math.log(n, 2)), 1, -1):
+            k = int(n**m**-1)
+            if (k**(m + 1) - 1) // (k - 1) == n:
+                return str(k)
+
+        return str(n - 1)
