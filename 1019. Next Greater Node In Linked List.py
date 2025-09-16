7e3602412c8a9450fca96ff6586c464996dd0870
arun-r-007 <arunakhil9715@gmail.com>
2025-09-16 20:20:44 +0530

    LeetCode

---
+++ 1019. Next Greater Node In Linked List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def nextLargerNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: List[int]
+        """
+        ls = []
+        current = head
+        while current:
+            ls.append(current.val)
+            current = current.next
+        
+        result = [0] * len(ls)
+        stack = []  # stack holds indices
+        
+        for i, value in enumerate(ls):
+            while stack and ls[stack[-1]] < value:
+                index = stack.pop()
+                result[index] = value
+            stack.append(i)
+        
+        return result
