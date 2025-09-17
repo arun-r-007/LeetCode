8f361f1eb90f2b6317e3fbc39e1eb9b572d753b3
arun-r-007 <arunakhil9715@gmail.com>
2025-09-17 20:23:19 +0530

    LeetCode

---
+++ 1721. Swapping Nodes in a Linked List.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def swapNodes(self, head, k):
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+        
+        ls = []
+        current = head
+        while current:
+            ls.append(current)
+            current = current.next
+        
+        n = len(ls)
+        
+        
+        node1 = ls[k-1]
+        node2 = ls[n-k]
+        
+        node1.val, node2.val = node2.val, node1.val
+        
+        return head
