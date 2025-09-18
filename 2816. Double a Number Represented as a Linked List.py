4a5c327e2078091b65c4d9770ab5df6821d9a893
arun-r-007 <arunakhil9715@gmail.com>
2025-09-18 23:52:48 +0530

    LeetCode

--- 2816. Double a Number Represented as a Linked List.py
+++ 2816. Double a Number Represented as a Linked List.py
@@ -1,41 +1,113 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 class Solution(object):
+    def reverseList(self, head):
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+    
     def doubleIt(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if head.next is None and head.val <= 4 :
-            head.val = head.val * 2
-            return head
+        head = self.reverseList(head)
 
-        ls = []
-        current = head
-        while current is not None :
-            ls.append(current.val)
-            current = current.next
+        carry = 0
+        curr = head
+        prev = None
+        while curr:
+            val = curr.val * 2 + carry
+            curr.val = val % 10
+            carry = val // 10
+            prev = curr
+            curr = curr.next
+
+        if carry:
+            prev.next = ListNode(carry)
+
+        return self.reverseList(head)
 
 
-        n = len(ls)
 
-        singleNumber = int("".join(map(str,ls)))
 
-        head = None
 
-        singleNumber = singleNumber * 2
 
-        ls = []
-        ls = [int(d) for d in str(singleNumber)]
 
-        dummy = ListNode(0)
-        current = dummy
-        for val in ls:
-            current.next = ListNode(val)
-            current = current.next
 
-        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution(object):
+#     def doubleIt(self, head):
+#         """
+#         :type head: Optional[ListNode]
+#         :rtype: Optional[ListNode]
+#         """
+#         if head.next is None and head.val <= 4 :
+#             head.val = head.val * 2
+#             return head
+
+#         ls = []
+#         current = head
+#         while current is not None :
+#             ls.append(current.val)
+#             current = current.next
+
+
+#         n = len(ls)
+
+#         singleNumber = int("".join(map(str,ls)))
+
+#         head = None
+
+#         singleNumber = singleNumber * 2
+
+#         ls = []
+#         ls = [int(d) for d in str(singleNumber)]
+
+#         dummy = ListNode(0)
+#         current = dummy
+#         for val in ls:
+#             current.next = ListNode(val)
+#             current = current.next
+
+#         return dummy.next
         
