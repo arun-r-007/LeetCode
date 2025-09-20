67e9456914c76294e584927850957e8cca213a92
arun-r-007 <arunakhil9715@gmail.com>
2025-09-20 20:52:35 +0530

    LeetCode

--- 2095. Delete the Middle Node of a Linked List.py
+++ 2095. Delete the Middle Node of a Linked List.py
@@ -9,34 +9,109 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        if head.next is None:
+            return None
 
-        if head.next is None :
-            head = None
-            return head
+        slow = head
+        fast = head
+        prev = None
 
-        if head.next.next is None :
-            head.next = None
-            return head
-        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
 
-        current = head
-        ls = []
-
-        while current is not None :
-            ls.append(current.val)
-            current = current.next
-
-        n = len(ls)
-        target = ( n // 2 ) + 1 
-
-        prev = head
-        current = head
-
-        for i in range(1,target):
-            prev = current
-            current = current.next
-
-        prev.next = current.next
+        prev.next = slow.next
 
         return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Definition for singly-linked list.
+# # class ListNode(object):
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution(object):
+#     def deleteMiddle(self, head):
+#         """
+#         :type head: Optional[ListNode]
+#         :rtype: Optional[ListNode]
+#         """
+
+#         if head.next is None :
+#             head = None
+#             return head
+
+#         if head.next.next is None :
+#             head.next = None
+#             return head
+        
+
+#         current = head
+#         ls = []
+
+#         while current is not None :
+#             ls.append(current.val)
+#             current = current.next
+
+#         n = len(ls)
+#         target = ( n // 2 ) + 1 
+
+#         prev = head
+#         current = head
+
+#         for i in range(1,target):
+#             prev = current
+#             current = current.next
+
+#         prev.next = current.next
+
+#         return head
         
