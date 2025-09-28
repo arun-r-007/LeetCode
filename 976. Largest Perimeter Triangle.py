d6d08a16aada9baaf1674160f2735d28adbeb1a6
arun-r-007 <arunakhil9715@gmail.com>
2025-09-29 05:23:12 +0530

    LeetCode

--- 976. Largest Perimeter Triangle.py
+++ 976. Largest Perimeter Triangle.py
@@ -4,4 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        nums.sort(reverse=True)
+        for i in range(len(nums) - 2):
+            if nums[i+1] + nums[i+2] > nums[i]:
+                return nums[i] + nums[i+1] + nums[i+2]
+        return 0
