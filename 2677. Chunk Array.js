6f51de4473d77d61c484284fe6a77b2728c10339
arun-r-007 <arunakhil9715@gmail.com>
2025-10-07 20:28:42 +0530

    LeetCode

--- 2677. Chunk Array.js
+++ 2677. Chunk Array.js
@@ -4,21 +4,62 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
+    if (!arr.length || size <= 0) return [];
 
-    if (!arr.length || size <= 0)
-    {
-        return [];
+    const result = [];
+    let temp = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        temp.push(arr[i]);
+
+        // when chunk reaches the given size, push and reset
+        if (temp.length === size) {
+            result.push(temp);
+            temp = [];
+        }
     }
 
-    const split_array = [];
-    const n = arr.length;
+    // add remaining elements
+    if (temp.length) result.push(temp);
 
-    for (let i = 0 ;i < n; i += size)
-    {
-        const slice1 = arr.slice(i, i + size);
-        split_array.push(slice1);
-    }
-
-    return split_array;
-    
+    return result;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * @param {Array} arr
+//  * @param {number} size
+//  * @return {Array}
+//  */
+// var chunk = function(arr, size) {
+
+//     if (!arr.length || size <= 0)
+//     {
+//         return [];
+//     }
+
+//     const split_array = [];
+//     const n = arr.length;
+
+//     for (let i = 0 ;i < n; i += size)
+//     {
+//         const slice1 = arr.slice(i, i + size);
+//         split_array.push(slice1);
+//     }
+
+//     return split_array;
+    
+// };
