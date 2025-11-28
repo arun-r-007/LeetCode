c60831078678075d11c93800bbcf7d305383aac7
arun-r-007 <arunakhil9715@gmail.com>
2025-11-28 09:48:21 +0530

    LeetCode

---
+++ 561. Array Partition.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        b = int(len(nums))
+        sum = 0
+        for i in range(0,b,2):
+            sum = sum + min(nums[i],nums[i+1])
+        return sum
+
+        
+
+a = Solution() 
+print(a.arrayPairSum([1,4,3,2]))
+
+
+
+
+
+# class Solution:
+#   def arrayPairSum(self, nums: list[int]) -> int:
+#     return sum(sorted(nums)[::2])
