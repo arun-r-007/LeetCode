cd90fc99dfeafd9ec0200f79664e0de27e2b4521
arun-r-007 <arunakhil9715@gmail.com>
2025-12-14 12:52:57 +0530

    LeetCode

--- 1. Two Sum.c
+++ 1. Two Sum.c
@@ -1,19 +1,33 @@
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* result = (int*)malloc(2 * sizeof(int));
-    for(int i=0;i<numsSize;i++)
-    {
-        for(int j=i+1;j<numsSize;j++)
-        {
-            if(nums[i]+nums[j]==target)
-            {
+    *returnSize = 2;
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
                 result[0] = i;
                 result[1] = j;
-                *returnSize = 2;
                 return result;
             }
         }
     }
-    *returnSize = 0; 
     return NULL;
+}
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+
+    int* res = twoSum(nums, 4, target, &returnSize);
+
+    printf("[%d, %d]\n", res[0], res[1]);
+    free(res);
+
+    system("pause");
+
+    return 0;
 }
