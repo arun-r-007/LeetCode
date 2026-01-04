472e3936d9950260f351622f60ed51d6a44c10c7
arun-r-007 <arunakhil9715@gmail.com>
2026-01-04 20:15:30 +0530

    LeetCode

--- SQL/1251. Average Selling Price.sql
+++ SQL/1251. Average Selling Price.sql
@@ -15,4 +15,11 @@
 
 
 
-
+SELECT
+a.product_id
+,round(coalesce((SUM(a.price*b.units)/sum(b.units)),0),2) AS average_price
+FROM Prices a
+LEFT JOIN UnitsSold b
+ON a.product_id = b.product_id
+AND b.purchase_date >= a.start_date AND b.purchase_date <= a.end_date
+GROUP BY 1
