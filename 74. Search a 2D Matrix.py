fd9623591c9daccc7c54795c270b12369718ebcd
arun-r-007 <arunakhil9715@gmail.com>
2026-01-05 20:43:12 +0530

    LeetCode

---
+++ 74. Search a 2D Matrix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+                return False
+
+        for row in matrix:
+            if row[0] <= target <= row[-1]:
+                left, right = 0, len(row) - 1
+                while left <= right:
+                    mid = (left + right) // 2
+                    if row[mid] == target:
+                        return True
+                    elif row[mid] < target:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+
+        return False
