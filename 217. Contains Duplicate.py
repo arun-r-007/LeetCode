03de8d9d1afdc3fa542a0aebb636f227d959b56b
arun-r-007 <arunakhil9715@gmail.com>
2026-01-05 12:30:23 +0530

    LeetCode

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -8,15 +8,15 @@ class Solution(object):
 
 
 
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         seen = set()
+#         for num in nums:
+#             if num in seen:
+#                 return True
+#             seen.add(num)
+#         return False
