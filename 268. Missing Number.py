7eb77cd94c6f8ac72cb20b14569cdc4498ae086f
arun-r-007 <arunakhil9715@gmail.com>
2026-01-05 10:09:12 +0530

    LeetCode

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -8,3 +8,20 @@ class Solution(object):
         expected = n * (n + 1) // 2
         actual = sum(nums)
         return expected - actual
+    
+
+
+# class Solution(object):
+#     def missingNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         nums.sort()
+#         n = len(nums)
+
+#         for i in range(n):
+#             if i != nums[i]:
+#                 return i
+
+#         return n
