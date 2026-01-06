2fb208a7900b36490362ffbaa79e46fe822b5163
arun-r-007 <arunakhil9715@gmail.com>
2026-01-06 15:09:47 +0530

    LeetCode

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -4,10 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_sum = curr_sum = nums[0]
-        n = len(nums)
-        for i in range(1, n):
-            curr_sum = max(nums[i], curr_sum + nums[i])
-            max_sum = max(max_sum, curr_sum)
+        sum = 0
+        max_sum = float("-inf")
+        for i in nums:
+            if sum < 0 :sum = 0
+            sum+=i
+            max_sum = max(max_sum,sum)
 
         return max_sum
+
+
+
+
+
+
+# class Solution(object):
+#     def maxSubArray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         max_sum = curr_sum = nums[0]
+#         n = len(nums)
+#         for i in range(1, n):
+#             curr_sum = max(nums[i], curr_sum + nums[i])
+#             max_sum = max(max_sum, curr_sum)
+
+#         return max_sum
