3ffb86167bfbaf11f8c14d7328f07ef8d7471179
arun-r-007 <arunakhil9715@gmail.com>
2026-01-06 09:53:47 +0530

    LeetCode

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -8,10 +8,42 @@ class Solution(object):
             return False
          
         string = str(x)
-        string_rev = string[::-1]
+        # string_rev = string[::-1]
 
-        if string == string_rev:
-            return True
-        else:
-            return False 
+        # if string == string_rev:
+        #     return True
+        # else:
+        #     return False
+        n = len(string)//2
+
+        for i in range(0,n):
+            if string[i] == string[-i-1]:
+                continue
+            else : return False
+        
+        return True
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def isPalindrome(self, x):
+#         """
+#         :type x: int
+#         :rtype: bool
+#         """
+#         if x < 0:
+#             return False
+         
+#         string = str(x)
+#         string_rev = string[::-1]
+
+#         if string == string_rev:
+#             return True
+#         else:
+#             return False 
         
