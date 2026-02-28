3ae7776e86d17cd4b1f3441e1005b29f448dcb61
arun-r-007 <arunakhil9715@gmail.com>
2026-02-28 21:30:30 +0530

    LeetCode

--- 1171. Remove Zero Sum Consecutive Nodes from Linked List.py
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List.py
@@ -10,34 +10,92 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
 
-        # Step 1: Convert linked list to array
-        arr = []
-        curr = head
-        while curr:
-            arr.append(curr.val)
-            curr = curr.next
-        
-        # Step 2: Remove zero sum subarrays using stack
-        stack = []
-        
-        for num in arr:
-            stack.append(num)
-            
-            # Check if any ending subarray sums to 0
-            total = 0
-            for i in range(len(stack) - 1, -1, -1):
-                total += stack[i]
-                if total == 0:
-                    # Remove that zero sum part
-                    stack = stack[:i]
-                    break
-        
-        # Step 3: Convert stack back to linked list
         dummy = ListNode(0)
+        dummy.next = head
+        premap = {}
         curr = dummy
-        
-        for num in stack:
-            curr.next = ListNode(num)
+        prefix = 0
+        while curr:
+            prefix += curr.val
+            premap[prefix] = curr
             curr = curr.next
-        
+        curr = dummy
+        prefix = 0
+        while curr:
+            prefix += curr.val
+            curr.next = premap[prefix].next
+            curr = curr.next
         return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Definition for singly-linked list.
+# # class ListNode(object):
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution(object):
+#     def removeZeroSumSublists(self, head):
+#         """
+#         :type head: Optional[ListNode]
+#         :rtype: Optional[ListNode]
+#         """
+
+#         # Step 1: Convert linked list to array
+#         arr = []
+#         curr = head
+#         while curr:
+#             arr.append(curr.val)
+#             curr = curr.next
+        
+#         # Step 2: Remove zero sum subarrays using stack
+#         stack = []
+        
+#         for num in arr:
+#             stack.append(num)
+            
+#             # Check if any ending subarray sums to 0
+#             total = 0
+#             for i in range(len(stack) - 1, -1, -1):
+#                 total += stack[i]
+#                 if total == 0:
+#                     # Remove that zero sum part
+#                     stack = stack[:i]
+#                     break
+        
+#         # Step 3: Convert stack back to linked list
+#         dummy = ListNode(0)
+#         curr = dummy
+        
+#         for num in stack:
+#             curr.next = ListNode(num)
+#             curr = curr.next
+        
+#         return dummy.next
