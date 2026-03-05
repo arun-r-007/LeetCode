ea882117de1bbb3f86df2db879a679917f1c6283
arun-r-007 <arunakhil9715@gmail.com>
2026-03-05 22:38:21 +0530

    LeetCode

--- 190. Reverse Bits.py
+++ 190. Reverse Bits.py
@@ -4,7 +4,36 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        str1 = bin(n)[2:]
-        str1 = str1.zfill(32)
-        str1 = str1[::-1]
-        return int(str1, 2)
+        res = 0
+        
+        for _ in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def reverseBits(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         str1 = bin(n)[2:]
+#         str1 = str1.zfill(32)
+#         str1 = str1[::-1]
+#         return int(str1, 2)
