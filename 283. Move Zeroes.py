6264c59f0b22ed23e6936490c3bd336d14b7be4e
arun-r-007 <arunakhil9715@gmail.com>
2026-03-07 20:17:47 +0530

    LeetCode

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,20 +1,40 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        count = 0
-        reach = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def moveZeroes(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: None Do not return anything, modify nums in-place instead.
+#         """
+#         count = 0
+#         reach = 0
         
-        for i in nums:
-            if i == 0:
-                count += 1
+#         for i in nums:
+#             if i == 0:
+#                 count += 1
 
-        nums += [0] * count
+#         nums += [0] * count
 
-        for i in nums:
-            if i == 0 and reach < count :
-                nums.remove(i)
-                reach += 1
+#         for i in nums:
+#             if i == 0 and reach < count :
+#                 nums.remove(i)
+#                 reach += 1
         
