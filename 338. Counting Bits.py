4c659f88c8881ff5fb87703589ae843144c32297
arun-r-007 <arunakhil9715@gmail.com>
2026-03-09 12:04:17 +0530

    LeetCode

--- 338. Counting Bits.py
+++ 338. Counting Bits.py
@@ -1,17 +1,40 @@
-class Solution(object):
-    def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        lst = []
-        result = []
 
-        for i in range(n+1):
-            lst.append(int(bin(i)[2:]))
-            result.append(str(lst[i]).count('1'))
 
-        # print(lst)
-        # print(result)
 
-        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def countBits(self, n):
+#         """
+#         :type n: int
+#         :rtype: List[int]
+#         """
+#         lst = []
+#         result = []
+
+#         for i in range(n+1):
+#             lst.append(int(bin(i)[2:]))
+#             result.append(str(lst[i]).count('1'))
+
+#         # print(lst)
+#         # print(result)
+
+#         return result
