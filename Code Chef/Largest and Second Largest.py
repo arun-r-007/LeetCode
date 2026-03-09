922e03e8437b5bb144478ce2b746ed4bf9126b24
arun-r-007 <arunakhil9715@gmail.com>
2026-03-09 12:44:25 +0530

    LeetCode

--- Code Chef/Largest and Second Largest.py
+++ Code Chef/Largest and Second Largest.py
@@ -6,8 +6,50 @@ while t > 0:
     t -= 1
     # Your code goes here
     
-    x = max(a)
-    while x in a:
-        a.remove(x)
-    y = max(a)
-    print(x+y)
+    # x = max(a)
+    # while x in a:
+    #     a.remove(x)
+    # y = max(a)
+    # print(x+y)
+    
+    
+    largest = max(a)
+    second_largest = max(number for number in a if number != largest)
+    print(largest + second_largest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# t = int(input())
+
+# while t > 0:
+#     n = int(input())
+#     a = list(map(int, input().split()))
+#     t -= 1
+#     # Your code goes here
+    
+#     x = max(a)
+#     while x in a:
+#         a.remove(x)
+#     y = max(a)
+#     print(x+y)
