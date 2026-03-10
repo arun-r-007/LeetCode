4813c26f9799c545e7f17aeb5ad291c902e79466
arun-r-007 <arunakhil9715@gmail.com>
2026-03-10 20:56:16 +0530

    LeetCode

---
+++ 389. Find the Difference.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        result = 0
+        
+        for c in s + t:
+            result ^= ord(c)
+            
+        return chr(result)
