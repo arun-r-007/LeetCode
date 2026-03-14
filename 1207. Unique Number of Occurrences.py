5baa65b865fbd81ded5dbe352576894c78070975
arun-r-007 <arunakhil9715@gmail.com>
2026-03-14 08:14:27 +0530

    LeetCode

---
+++ 1207. Unique Number of Occurrences.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        dict1 = {}
+        set1 = set()
+
+        for i in arr:
+            if i in dict1:
+                dict1[i] += 1
+            else:
+                dict1[i] = 1
+
+        for i in dict1.values():
+            if i in set1:
+                return False
+            else:
+                set1.add(i)
+
+        return True
