d89de021e7d317d78bc0503bbb275859096cd4ab
arun-r-007 <arunakhil9715@gmail.com>
2026-03-19 21:46:39 +0530

    LeetCode

---
+++ Code Chef/Difficulty Rating Order.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+while t > 0:
+    n = int(input())
+    d = list(map(int, input().split()))
+    # Your code goes here
+    t -= 1
+    
+    is_sorted = True
+    
+    for i in range(n - 1):
+        if d[i] > d[i + 1]:
+            is_sorted = False
+            break
+    
+    if is_sorted:
+        print("YES")
+    else:
+        print("NO")
