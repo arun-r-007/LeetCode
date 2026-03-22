f506f2e55aff1e3d5502af1040d5e7da609d6fae
arun-r-007 <arunakhil9715@gmail.com>
2026-03-22 22:18:54 +0530

    LeetCode

--- Code Chef/Remove Outermost Parentheses.py
+++ Code Chef/Remove Outermost Parentheses.py
@@ -14,3 +14,19 @@ def remove_outer_parentheses(s):
                 res += ch
 
     return res
+
+# def remove_outer_parentheses(s):
+#     res = ""
+#     count = 0
+
+#     for ch in s:
+#         if ch == '(':
+#             if count > 0:
+#                 res += ch
+#             count += 1
+#         else:
+#             count -= 1
+#             if count > 0:
+#                 res += ch
+
+#     return res
