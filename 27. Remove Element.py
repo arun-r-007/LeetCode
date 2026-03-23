74942d97b3df8ec7d235ba6d6b4e978ce784382d
arun-r-007 <arunakhil9715@gmail.com>
2026-03-23 21:13:08 +0530

    LeetCode

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -8,3 +8,20 @@ class Solution(object):
         nums[:] = [x for x in nums if x != val]
         return len(nums)
     
+
+
+# class Solution(object):
+#     def removeElement(self, nums, val):
+#         """
+#         :type nums: List[int]
+#         :type val: int
+#         :rtype: int
+#         """
+#         k = 0
+
+#         for i in range(len(nums)):
+#             if nums[i] != val:
+#                 nums[k] = nums[i]
+#                 k += 1
+
+#         return k
