78ebb9ca3ee11f13af43b55df4454172a3f8684f
arun-r-007 <arunakhil9715@gmail.com>
2026-03-23 21:39:53 +0530

    LeetCode

--- Code Chef/Add One.py
+++ Code Chef/Add One.py
@@ -1,9 +1,46 @@
-# cook your dish here
 t = int(input())
 
 while t:
     t -= 1
     
-    n = int(input())
+    num = input().strip()
     
-    print(n+1)
+    num = list(num)
+    i = len(num) - 1
+    
+    while i >= 0:
+        if num[i] == '9':
+            num[i] = '0'
+            i -= 1
+        else:
+            num[i] = str(int(num[i]) + 1)
+            break
+    else:
+        num.insert(0, '1')   # all digits were 9
+    
+    print("".join(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # cook your dish here
+# t = int(input())
+
+# while t:
+#     t -= 1
+    
+#     n = int(input())
+    
+#     print(n+1)
