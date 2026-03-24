32830b2e2c9c8ca4e7747ae968c8e5b85542eb0a
arun-r-007 <arunakhil9715@gmail.com>
2026-03-24 22:48:43 +0530

    LeetCode

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -4,14 +4,41 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dict1 = {}
 
-        for i in nums:
-            if i in dict1:
-                dict1[i] += 1
-            else:
-                dict1[i] = 1
+        nums.sort()
+        n=len(nums)
+        return nums[n//2]
 
-        key = max(dict1, key=dict1.get)
 
-        return key
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def majorityElement(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         dict1 = {}
+
+#         for i in nums:
+#             if i in dict1:
+#                 dict1[i] += 1
+#             else:
+#                 dict1[i] = 1
+
+#         key = max(dict1, key=dict1.get)
+
+#         return key
