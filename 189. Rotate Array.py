d5ffbc2379d7057db4139ad7f16e16cd181ca468
arun-r-007 <arunakhil9715@gmail.com>
2026-03-25 06:29:01 +0530

    LeetCode

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -1,17 +1,55 @@
-class Solution(object):
-    def rotate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        k = k % n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+
+# class Solution(object):
+#     def rotate(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: None Do not return anything, modify nums in-place instead.
+#         """
+
+
+#         d = deque(nums)
+
+#         d.rotate(k)
+
+#         nums[:] = list(d)
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def rotate(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: None Do not return anything, modify nums in-place instead.
+#         """
+#         n = len(nums)
+#         k = k % n
         
-        end_val = nums[-k:]
+#         end_val = nums[-k:]
         
-        for i in range(n - k - 1, -1, -1):
-            nums[i + k] = nums[i]
+#         for i in range(n - k - 1, -1, -1):
+#             nums[i + k] = nums[i]
         
-        for i in range(k):
-            nums[i] = end_val[i]
+#         for i in range(k):
+#             nums[i] = end_val[i]
