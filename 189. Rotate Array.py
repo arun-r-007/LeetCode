8ca4549850921f15b9ab138c913dd625f8645e43
arun-r-007 <arunakhil9715@gmail.com>
2026-03-25 06:32:25 +0530

    LeetCode

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -1,8 +1,13 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
 
-
-
-
-
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
 
 
 
