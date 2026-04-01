66155719faf97c8625461e35ee9620b2f3d415ca
arun-r-007 <arunakhil9715@gmail.com>
2026-04-01 22:37:32 +0530

    LeetCode

--- Leet Code/14. Longest Common Prefix.py
+++ Leet Code/14. Longest Common Prefix.py
@@ -1,4 +1,20 @@
-
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+        s1 = min(strs)
+        s2 = max(strs)
+        
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                return s1[:i]
+        
+        return s1
 
 
 
