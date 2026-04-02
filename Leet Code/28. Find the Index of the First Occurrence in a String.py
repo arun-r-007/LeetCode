369e37d3dae678988e4224ee46afbdb9694114bd
arun-r-007 <arunakhil9715@gmail.com>
2026-04-02 21:17:20 +0530

    LeetCode

--- Leet Code/28. Find the Index of the First Occurrence in a String.py
+++ Leet Code/28. Find the Index of the First Occurrence in a String.py
@@ -1,21 +1,41 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        n, m = len(haystack), len(needle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def strStr(self, haystack, needle):
+#         """
+#         :type haystack: str
+#         :type needle: str
+#         :rtype: int
+#         """
+#         n, m = len(haystack), len(needle)
         
-        if m == 0:
-            return 0
+#         if m == 0:
+#             return 0
         
-        # sliding window
-        for i in range(n - m + 1):
-            if haystack[i:i+m] == needle:
-                return i
+#         # sliding window
+#         for i in range(n - m + 1):
+#             if haystack[i:i+m] == needle:
+#                 return i
         
-        return -1
+#         return -1
     
 
-print(Solution().strStr("sadbutsad", "sad"))
+# print(Solution().strStr("sadbutsad", "sad"))
