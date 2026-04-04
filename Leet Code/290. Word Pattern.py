2fd8d08852efeb721142663d07ab2802311f34bf
arun-r-007 <arunakhil9715@gmail.com>
2026-04-04 22:16:07 +0530

    LeetCode

--- Leet Code/290. Word Pattern.py
+++ Leet Code/290. Word Pattern.py
@@ -1,4 +1,16 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
 
+
+        words = s.split()
+        
+        return len(pattern) == len(words) and \
+               len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words))
 
 
 
