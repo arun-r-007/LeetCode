0d7ffa747e7f2c097b1774e577b9483d09fa1657
arun-r-007 <arunakhil9715@gmail.com>
2026-04-07 21:49:25 +0530

    LeetCode

--- Leet Code/20. Valid Parentheses.py
+++ Leet Code/20. Valid Parentheses.py
@@ -1,3 +1,29 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {'(': ')', '[': ']', '{': '}'}
+        
+        for ch in s:
+            if ch in mapping:
+                stack.append(mapping[ch])
+            else:
+                if not stack or stack.pop() != ch:
+                    return False
+        
+        return not stack
+
+
+
+
+
+
+
+
+
 
 
 
