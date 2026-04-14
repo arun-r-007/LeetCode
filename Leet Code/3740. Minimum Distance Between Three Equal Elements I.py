f429c64498efdad3092d775e7fe293c5b41d6b5d
arun-r-007 <arunakhil9715@gmail.com>
2026-04-14 18:28:52 +0530

    LeetCode

--- Leet Code/3740. Minimum Distance Between Three Equal Elements I.py
+++ Leet Code/3740. Minimum Distance Between Three Equal Elements I.py
@@ -1,4 +1,34 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        ans = float('inf')
 
+        dict1 = {}
+
+        for i in range(len(nums)):
+            if nums[i] in dict1:
+                dict1[nums[i]].append(i)
+            else:
+                dict1[nums[i]] = [i]  
+
+
+        for key in dict1:
+            indices = dict1[key]
+
+            if len(indices) >= 3:
+                for i in range(len(indices) - 2):
+                    a = indices[i]
+                    b = indices[i+1]
+                    c = indices[i+2]
+
+                    temp = abs(a - b) + abs(b - c) + abs(c - a)
+                    ans = min(ans, temp)
+
+        return ans if ans != float('inf') else -1
 
 
 
