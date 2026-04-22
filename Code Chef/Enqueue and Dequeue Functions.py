62dbc11f8c593f3a1faabe2d3a15435a207d8f4b
arun-r-007 <arunakhil9715@gmail.com>
2026-04-22 23:43:47 +0530

    CodeChef

---
+++ Code Chef/Enqueue and Dequeue Functions.py
@@ -0,0 +1,33 @@
+MAX_SIZE = 101
+a = [0] * MAX_SIZE
+front = 0
+rear = -1
+current_size = 0
+
+def isEmpty():
+    return current_size == 0
+
+def isFull():
+    return current_size == MAX_SIZE
+
+def size():
+    return current_size
+
+def enqueue(item):
+    global rear, current_size
+    if isFull():
+        print("Queue is full. Cannot enqueue.")
+        return
+    rear = (rear + 1) % MAX_SIZE
+    a[rear] = item
+    current_size += 1
+
+def dequeue():
+    global front, current_size
+    if isEmpty():
+        print("Queue is empty. Cannot dequeue.")
+        return -1
+    removed_item = a[front]
+    front = (front + 1) % MAX_SIZE
+    current_size -= 1
+    return removed_item
