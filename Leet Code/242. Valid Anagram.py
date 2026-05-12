eaf4dad88b3e4e2485a31d54c233fcef22737764
arun-r-007 <arunakhil9715@gmail.com>
2026-05-12 23:06:51 +0530

    LeetCode

--- Leet Code/242. Valid Anagram.py
+++ Leet Code/242. Valid Anagram.py
@@ -5,4 +5,44 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for i in s:
+            count[i] = count.get(i, 0) + 1
+
+        for i in t:
+            count[i] = count.get(i, 0) - 1
+
+        for value in count.values():
+            if value != 0:
+                return False
+
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def isAnagram(self, s, t):
+#         """
+#         :type s: str
+#         :type t: str
+#         :rtype: bool
+#         """
+#         return sorted(s) == sorted(t)
